90e169aec7998b19b1621b4b80bcb731637a41ff
sumitkakshapati <me.summet@gmail.com>
2023-01-14 18:08:42 +0545

    cart shadow added

--- lib/features/cart/ui/widgets/cart_widget.dart
+++ lib/features/cart/ui/widgets/cart_widget.dart
@@ -4,6 +4,7 @@ import 'package:ecommerce_app/features/checkout/ui/screens/check_out_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:page_transition/page_transition.dart';
 
 class CartWidget extends StatelessWidget {
@@ -27,16 +28,44 @@ class CartWidget extends StatelessWidget {
             horizontal: 12,
             vertical: 8,
           ),
-          child: CustomRoundedButtom(
-            title: "Checkout",
-            onPressed: () {
-              Navigator.of(context).push(
-                PageTransition(
-                  child: CheckoutPage(),
-                  type: PageTransitionType.fade,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(1, -3),
+                color: Colors.grey.withOpacity(0.2),
+                blurRadius: 10,
+                spreadRadius: 10,
+              )
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 6),
+                child: Text(
+                  "Total Cost: Rs. 2000000",
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              );
-            },
+              ),
+              SizedBox(height: 4),
+              CustomRoundedButtom(
+                title: "Checkout",
+                onPressed: () {
+                  Navigator.of(context).push(
+                    PageTransition(
+                      child: CheckoutPage(),
+                      type: PageTransitionType.fade,
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
         )
       ],
